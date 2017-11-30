0a0f6b852d06a8894f092c75a4731bba1e1a8ccf
Jose Miguel Maduro <maduro.josem@gmail.com>
2017-11-30 06:44:51 -0400

    add test for contacts

--- test/controllers/v1/contacts_controller_test.rb
+++ test/controllers/v1/contacts_controller_test.rb
@@ -1,7 +1,23 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
-class V1::ContactsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+module V1
+  class ContactsControllerTest < ActionDispatch::IntegrationTest
+    test 'should get only contact for artellectual' do
+      account = accounts(:artellectual)
+
+      not_artellectial_contact = contacts(:two)
+
+      get(
+        v1_contacts_path(account_id: account.id)
+      )
+
+      contacts = JSON.parse(@response.body)['data']
+      contact_ids = contacts.map { |contact| contact['id'] }
+
+      assert_response :success
+      assert_not_includes contact_ids, not_artellectial_contact.id
+    end
+  end
 end
